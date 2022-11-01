703f47013bece358515c711be2c79640d0202f9c
GAOSHIQI <GhaSiKey@users.noreply.huggingface.co>
2022-11-01 08:28:13 +0000

    Create app.py

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 import gradio as gr
+import torch
+import requests
+from torchvision import transforms
 
-def greet(name):
-    return "Hello " + name + "!!"
+model = torch.hub.load('pytorch/vision:v0.6.0', 'resnet18', pretrained=True).eval()
+response = requests.get("https://git.io/JJkYN")
+labels = response.text.split("\n")
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+def predict(inp):
+  inp = transforms.ToTensor()(inp).unsqueeze(0)
+  with torch.no_grad():
+    prediction = torch.nn.functional.softmax(model(inp)[0], dim=0)
+    confidences = {labels[i]: float(prediction[i]) for i in range(1000)}    
+  return confidences
+
+demo = gr.Interface(fn=predict, 
+             inputs=gr.inputs.Image(type="pil"),
+             outputs=gr.outputs.Label(num_top_classes=3),
+             examples=[["cheetah.jpg"]],
+             )
+             
+demo.launch()
